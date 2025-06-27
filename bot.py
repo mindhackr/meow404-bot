327934b7bdc08736d6cb82432a8ec54244c2ec96
mindhackr <niko.kozhushko@gmail.com>
2025-06-27 21:41:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-API_TOKEN = "ВАШ_ТОКЕН_ЗДЕСЬ"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.getenv("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
